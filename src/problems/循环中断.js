21034e230010e6fec579334b459555caf3fce408
gogo <xiaotian.zhang@56qq.com>
2019-02-22 09:52:56 +0800

    feat: 循环中断

---
+++ src/problems/循环中断.js
@@ -0,0 +1,52 @@
+const arr = [1, 2, 3];
+
+arr.forEach((x) => {
+  if (x === 1) {
+    console.log('forEach return success!');
+    return;
+  }
+  console.log('forEach return failed!');
+});
+
+arr.forEach((x) => {
+  if (x === 1) {
+    console.log('forEach break success!');
+    // break; // SyntaxError: Illegal break statement
+  }
+  console.log('forEach break failed!');
+});
+
+arr.map((x) => {
+  if (x === 1) {
+    console.log('map return success!');
+    return;
+  }
+  console.log('map return failed!');
+});
+
+arr.map((x) => {
+  if (x === 1) {
+    console.log('map return success!');
+    // break; // SyntaxError: Illegal break statement
+  }
+  console.log('map return failed!');
+});
+
+
+// for of 可以对循环进行中断
+for (let y of arr) {
+  if (y === 1) {
+    console.log('for of break success!');
+    break;
+  }
+  console.log('for of break failed!');
+}
+
+for (let x of arr) {
+  if (x === 1) {
+    console.log('for of return success!');
+    return;
+  }
+  console.log('for of return failed!');
+}
+
