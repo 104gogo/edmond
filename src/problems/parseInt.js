4199f7d5bd4ee4e41b2127e05528d6e25efaed6c
gogo <xiaotian.zhang@56qq.com>
2019-02-22 10:09:24 +0800

    feat: 添加parseInt的变形题

--- src/problems/parseInt.js
+++ src/problems/parseInt.js
@@ -1,3 +1,8 @@
 ['1', '2', '3'].map(Number); // 正确
 
 ['1', '2', '3'].map(parseInt); // 错误，parseInt 接受两个参数，第二个参数表示进制，第一个参数根据第二个参数确定是什么进制
+
+// 变形
+const unary = fn => val => fn(val);
+const parse = unary(parseInt);
+console.log(['1.1', '2', '0.3'].map(parse));
