4fc04d731417c6e00d66c1c6574c7a67f7beefbe
gogo <xiaotian.zhang@56qq.com>
2019-03-06 13:59:13 +0800

    feat: 添加prototype.js

---
+++ src/problems/prototype.js
@@ -0,0 +1,16 @@
+function A() {}
+function B() {}
+Object.prototype.aa = () => console.log('aa');
+Function.prototype.bb = () => console.log('bb');
+
+const objA = new A();
+const objB = new B();
+
+console.log(objA.aa()); // 'aa'
+console.log(A.bb()); // 'bb'
+console.log(B.aa()); // 'aa'
+console.log(objB.bb()); // Error
+
+// objB.__proto__ -> B.prototype -> Object.prototype -> null
+// B.__proto__ -> Function.prototype -> Object.prototype -> null
+// objA.__proto__ -> A.prototype -> Object.prototype -> null
