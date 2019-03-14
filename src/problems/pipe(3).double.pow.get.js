21775416146e57c698d3f78d3bec08f03ca8bc86
gogo <xiaotian.zhang@56qq.com>
2019-03-14 11:05:08 +0800

    feat: 添加proxy的实现

--- src/problems/pipe(3).double.pow.get.js
+++ src/problems/pipe(3).double.pow.get.js
@@ -1,4 +1,10 @@
-function pipe(num) {
+/**
+ * 思路1：
+ * 使用 Object.defineProperty 代理对象上面的属性
+ * 兼容到 IE9。IE8不完全支持，如下：
+ * Also supported in Internet Explorer 8, but only on DOM objects and with some non-standard behaviors.
+ */
+function pipe1(num) {
   let total = num;
   const result = {};
 
@@ -14,4 +20,35 @@ function pipe(num) {
   return result;
 }
 
-pipe(3).double.pow.get; // 36
+/**
+ * 思路2：
+ * 使用 Proxy 代理对象，而不是代理具体的属性
+ * IE 浏览器不支持，babel 也不支持转换
+ */
+function pipe2(num) {
+  const arr = [];
+  let total = num;
+
+  const options = {
+    double: result => result * 2,
+    pow : result => result * result,
+  }
+
+  const proxyObj = new Proxy({}, {
+    get(obj, prop) {
+      if (prop === 'get') {
+        total = arr.reduce((prev, cur, index) => arr[index](prev), total);
+        console.log(total);
+        return proxyObj;
+      }
+
+      arr.push(options[prop]);
+      return proxyObj;
+    },
+  });
+  return proxyObj;
+}
+
+pipe1(3).double.pow.get; // 36
+pipe1(3).pow.double.get; // 18
+
